e7883538698682a561018927fb0a741348a5df93
Tomasz Kajda <tomaszkajda4@gmail.com>
2023-02-20 17:10:14 +0100

    parsing value from NBP api

--- backend/src/main/java/com/backend/controllers/CurrencyController.java
+++ backend/src/main/java/com/backend/controllers/CurrencyController.java
@@ -1,22 +1,33 @@
 package com.backend.controllers;
 
 
+import com.backend.services.CurrencyService;
 import com.google.gson.Gson;
+import org.json.JSONException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class CurrencyController {
 
     private final Gson gson = new Gson();
+    private final CurrencyService currencyService;
+
+    public CurrencyController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
 
     @CrossOrigin
     @RequestMapping(value = "/rate", method = RequestMethod.GET)
     public ResponseEntity<String> getCurrentRate() {
-        return ResponseEntity.ok().body(gson.toJson("4.56"));
+        try {
+            return ResponseEntity.ok().body(gson.toJson(currencyService.getCurrentGbpRate()));
+        } catch (IOException | JSONException ex) {
+            ex.printStackTrace();
+        }
+        return ResponseEntity.status(404).body(gson.toJson("not found"));
     }
 }
--- backend/src/main/java/com/backend/services/CurrencyService.java
+++ backend/src/main/java/com/backend/services/CurrencyService.java
@@ -1,8 +1,28 @@
 package com.backend.services;
 
+import org.json.*;
 import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.net.*;
+import java.util.stream.Collectors;
 
 @Service
 public class CurrencyService {
 
+    public double getCurrentGbpRate() throws IOException, JSONException {
+        URL url = new URL("http://api.nbp.pl/api/exchangerates/rates/a/gbp/?format=json");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        switch (connection.getResponseCode()) {
+            case 200:
+            case 201:
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String string = br.lines().collect(Collectors.joining());
+
+                return new JSONObject(string).getJSONArray("rates").getJSONObject(0).getDouble("mid");
+        }
+        return 0;
+    }
 }
