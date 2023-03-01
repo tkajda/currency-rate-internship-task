3cff62143c43e9f4806ccde4fd974f711d2cd097
Tomasz Kajda <tomaszkajda4@gmail.com>
2023-03-01 18:49:57 +0100

    unit tests + improved js

---
+++ frontend/__tests__/calculateValue.test.js
@@ -0,0 +1,60 @@
+const { calculateValue, isCorrectData }  = require('../script');
+
+document.body.innerHTML = 
+  "<input min=\"0\" step=\"0.01\" id=\"gbp-field\">" +
+  "<input min=\"0\" step=\"0.01\" id=\"pln-field\">";
+
+const gbpField = document.getElementById('gbp-field');
+const plnField = document.getElementById('pln-field');
+  
+jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+describe('validateData test', () => {
+  it('validate string containing only letters', () => {
+    plnField.value = 'basb';
+    const result = isCorrectData('from-pln');
+    expect(result).toBe(false);
+  });
+
+  it('validate string containing letters and digits', () => {
+    plnField.value = '812a';
+    const result = isCorrectData('from-pln');
+    expect(result).toBe(false);
+  });
+
+  it('validate string contating only digits', () => {
+    gbpField.value = '812';
+    const result = isCorrectData('from-gbp');
+    expect(result).toBe(true);
+  });
+
+  it('validate empty string', () => {
+    gbpField.value = '';
+    const result = isCorrectData('from-gbp');
+    expect(result).toBe(true);
+  });
+
+  it('validate negative number', () => {
+    gbpField.value = '-15';
+    const result = isCorrectData('from-gbp');
+    expect(result).toBe(false);
+  });
+
+});
+
+
+describe('calculateValue test', () => {
+  it('calculateValue should convert from PLN to GBP correctly', () => {
+    plnField.value = 100;
+    window.plnGbpRate = 5.1118;
+    const result = calculateValue('from-pln');
+    expect(result).toBe(19.56);
+  });
+
+  it('calculateValue should convert from GBP to PLN correctly', () => {
+    gbpField.value = 19.1;
+    window.plnGbpRate = 5.4209;
+    const result = calculateValue('from-gbp');
+    expect(result).toBe(103.54);
+  });
+});
--- frontend/script.js
+++ frontend/script.js
@@ -1,66 +1,86 @@
-const url = 'http://localhost:8080/api/rate'
+const url = 'http://localhost:8080/api/rate';
 
 const plnGbpRateDisplay = document.getElementById('current-rate-display');
-
 const plnField = document.getElementById('pln-field');
 const gbpField = document.getElementById('gbp-field');
 const loader = document.getElementById('loader');
 
-let plnGbpRate = 0;
+//for testing purposes
+window.plnGbpRate = 0;
+
 let timeout;
 
-const calculateAndUpdate = (mode) => {
-    if (mode === 'from-pln') {
-        if (plnField.value < 0) {
-            alert('Please enter a positive number')
-            return;
-        }
-        gbpField.value = Math.round(plnField.value / plnGbpRate * 100) / 100;
-    } else if (mode === 'from-gbp') {
-        if (gbpField.value < 0) {
-            alert('Please enter a positive number')
-            return;
-        }
-        plnField.value = Math.round(gbpField.value * plnGbpRate * 100) / 100;
-    }
+const hasOnlyDigits = (string) => {
+  return /^\d+$/.test(string) && string.length > 0 ;
 }
 
-const showErrorMessage = (message) => {
+const isCorrectData = (mode) => {
+  const fieldValue = mode === 'from-pln' ? plnField.value : gbpField.value;
+
+  if (fieldValue.length > 0 && (fieldValue < 0 || !hasOnlyDigits(fieldValue))) {
+    alert('Please enter a positive number');
+    plnField.value = '';
+    gbpField.value = '';
+    return false;
+  }
+  return true;
+};
+
+const calculateValue = (mode) => {
+  let fieldValue = mode === 'from-pln' ? plnField.value : gbpField.value;
+  fieldValue = parseFloat(fieldValue);
+  let plnGbpRate = window.plnGbpRate;
+  
+  const result = mode === 'from-pln' ? fieldValue / plnGbpRate : fieldValue * plnGbpRate;
+
+  return Math.round(result * 100) / 100;
+}
+
+const updateValue =  (mode, val) => {
+  if (mode === 'from-pln') {
+    gbpField.value = val;
+  } else if (mode === 'from-gbp') {
+      plnField.value = val;
+  }
+}
+
+const handleErrorMessage = (message, mode) => {
   alert(message);
+  updateValue(mode, 'NaN');
   plnGbpRateDisplay.innerHTML = message;
 }
 
-const getRate = async (mode) => {
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+const fetchRate = async (mode) => {
+  const response = await fetch(url)
+  .catch(err => {
+    handleErrorMessage(err.message, mode);
+    return;
   });
 
-  if (!response.ok) {
-    showErrorMessage('Error sending request');
-    return;
-  }
-  const data = await response.json();
-
+  const data = response != undefined ? await response.json() : undefined;
   plnGbpRate = data;
-  plnGbpRateDisplay.innerHTML = data != undefined ? '1 GBP = ' + data + ' PLN' : 'Cannot find rate';
-  calculateAndUpdate(mode);
+
+  plnGbpRateDisplay.innerHTML = data != undefined ? '1 GBP = ' + data + ' PLN' : 'Cannot fetch exchange rate';
+  updateValue(mode, calculateValue(mode));
 }
 
 const debounce = (mode) => {
   clearTimeout(timeout);
-  loader.style.display = 'block';
-  timeout = setTimeout(() => {
-    getRate(mode);
-    loader.style.display = 'none';
-  }, 750);
+  if (isCorrectData(mode)) {
+    loader.style.display = 'block';
+    timeout = setTimeout(() => {
+      fetchRate(mode);
+      loader.style.display = 'none';
+    }, 750);
+  }
 }
 
-gbpField.addEventListener('input', () => debounce('from-gbp'));
-plnField.addEventListener('input', () => debounce('from-pln'));
-
-window.onload = async () => {
-    getRate();
+window.onload = () => {
+  if(typeof window !== 'undefined') {
+    gbpField.addEventListener('input', () => debounce('from-gbp'));
+    plnField.addEventListener('input', () => debounce('from-pln'));
+  }
+  fetchRate();
 }
+
+module.exports = { calculateValue, isCorrectData };
